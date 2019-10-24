76e09fc98c9cec101e97a38ea4608047732e0926
MrVerdier <ande5630@stud.kea.dk>
2019-10-24 10:04:55 +0200

    Auto-commit

---
+++ weatherapp/precache-manifest.8f864e687026da77f49c67b5f0f22dd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f04af256edfb58445d659ed6a7fa61b9",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "bf8ef41a43e5b6c2688d",
+    "url": "/weatherapp/static/css/main.c84224a7.chunk.css"
+  },
+  {
+    "revision": "c83e8e52a05c98db5279",
+    "url": "/weatherapp/static/js/2.e453c356.chunk.js"
+  },
+  {
+    "revision": "bf8ef41a43e5b6c2688d",
+    "url": "/weatherapp/static/js/main.d1d5b9b2.chunk.js"
+  },
+  {
+    "revision": "6addbc205185a49536cd",
+    "url": "/weatherapp/static/js/runtime-main.608b6d07.js"
+  }
+]);
--- weatherapp/service-worker.js
+++ weatherapp/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f1207124c16802fefcd44292ffa95a9a.js"
+  "/weatherapp/precache-manifest.8f864e687026da77f49c67b5f0f22dd6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weatherapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
