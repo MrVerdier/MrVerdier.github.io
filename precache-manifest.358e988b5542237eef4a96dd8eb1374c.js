1bbc2c77f3134427bd3f29f18cf9f5089eb73368
MrVerdier <ande5630@stud.kea.dk>
2019-10-24 09:33:12 +0200

    Auto-commit

---
+++ precache-manifest.358e988b5542237eef4a96dd8eb1374c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7670f0525d8a9edd57ec52600617a69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd32c75b6d2241d7e2d0",
+    "url": "/static/css/main.c84224a7.chunk.css"
+  },
+  {
+    "revision": "c83e8e52a05c98db5279",
+    "url": "/static/js/2.e453c356.chunk.js"
+  },
+  {
+    "revision": "fd32c75b6d2241d7e2d0",
+    "url": "/static/js/main.257b8ccd.chunk.js"
+  },
+  {
+    "revision": "f85b3131cb64cd9819ba",
+    "url": "/static/js/runtime-main.06ddb292.js"
+  }
+]);
