9df066ac2640acbf8cd6d5290c82e091c6f7d983
MrVerdier <ande5630@stud.kea.dk>
2019-10-24 09:49:52 +0200

    Auto-commit

---
+++ weatherapp/precache-manifest.a366dda31393da7ce5d9e7e4ba933d74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7a5c3976f821d65c9f38141cc0e81c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc76b6cb707f5c943aa3",
+    "url": "/static/css/main.c84224a7.chunk.css"
+  },
+  {
+    "revision": "c83e8e52a05c98db5279",
+    "url": "/static/js/2.e453c356.chunk.js"
+  },
+  {
+    "revision": "fc76b6cb707f5c943aa3",
+    "url": "/static/js/main.1cd7d566.chunk.js"
+  },
+  {
+    "revision": "f85b3131cb64cd9819ba",
+    "url": "/static/js/runtime-main.06ddb292.js"
+  }
+]);
